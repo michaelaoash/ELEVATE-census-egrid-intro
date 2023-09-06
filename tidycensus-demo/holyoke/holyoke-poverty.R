d3d23f4a18d47ad9656ed1850ed485f0c18ecd84
Michael Ash <michaelaoash@gmail.com>
2023-09-06 10:58:00 -0400

    Fixed 2010-2020 typo, changed linewidth

--- tidycensus-demo/holyoke/holyoke-poverty.R
+++ tidycensus-demo/holyoke/holyoke-poverty.R
@@ -9,6 +9,7 @@ options(tigris_use_cache = TRUE)
 library(sp)
 library(sf)
 options(width=400)
+options(tibble.width=Inf)
 options(rlang_backtrace_on_error = "none")
 ## Documentation https://walker-data.com/tidycensus/articles/basic-usage.html
 ## Get your own Census API Key
@@ -58,7 +59,7 @@ ma_blockgroups_acs2020 <- get_acs(year=2020, geography = "block group", state="M
 ma_blockgroups_acs2020 <- ma_blockgroups_acs2020 %>% mutate(
                                                          poor_families_with_children = B17010_004E + B17010_011E + B17010_017E,
                                                          families_with_children = B17010_004E + B17010_011E + B17010_017E + B17010_024E + B17010_031E + B17010_037E,
-                                                         percent_families_with_children_in_poverty = poor_families_with_children/families_with_children * 100
+                                                         poverty_rt_families_with_children = poor_families_with_children/families_with_children * 100
                                                          )
 
 ## Plot some maps and subset
@@ -69,7 +70,7 @@ ggplot(hampden_blockgroups_acs2020) + geom_sf()
 ## As it happens, Tracts can neatly distinguish Holyoke from surrounding cities and towns.
 holyoke_blockgroups_acs2020 <- filter(hampden_blockgroups_acs2020, substr(TRACTCE,1,4) %in% c("8114","8115","8116","8117","8118","8119","8120","8121"))
 ggplot(holyoke_blockgroups_acs2020) + geom_sf() + geom_sf_text(aes(label=GEOID))
-ggplot(holyoke_blockgroups_acs2020) + geom_sf(aes(fill=percent_families_with_children_in_poverty))
+ggplot(holyoke_blockgroups_acs2020) + geom_sf(aes(fill=poverty_rt_families_with_children))
 
 
 ## Add some decoration
@@ -78,7 +79,7 @@ ggplot(holyoke_blockgroups_acs2020) + geom_sf(aes(fill=percent_families_with_chi
 hampden_VTD_2020  <- voting_districts(state="MA", county="013")
 head(hampden_VTD_2020)
 ggplot(hampden_VTD_2020) + geom_sf() + geom_sf_text(aes(label=VTDST20))
-filter(hampden_VTD_2010,grepl('Holyoke', NAME20, ignore.case=TRUE ))
+filter(hampden_VTD_2020,grepl('Holyoke', NAME20, ignore.case=TRUE )) %>% print(n=Inf)
 holyoke_VTD_2020  <-    filter(hampden_VTD_2020, as.numeric(substr(VTDST20,1,6))>=930, as.numeric(substr(VTDST20,1,6))<=943)
 ggplot(holyoke_VTD_2020) + geom_sf() + geom_sf_text(aes(label=NAME20))
 
@@ -88,13 +89,15 @@ holyoke_VTD_2020 <- holyoke_VTD_2020 %>% mutate(
     group_by(ward) %>%
     summarize()
 
+ggplot(holyoke_VTD_2020) + geom_sf() + geom_sf_text(aes(label=ward))
+
 
 ## Limit roads to interstates and major roads in Providence
 ## roads  <- st_read("tl_25013/tl_2022_25013_roads.shp") %>% filter(MTFCC %in% c("S1100","S1200"))
 hampden_roads <- roads(state="MA", county="013") 
 
 ## s2::s2_options(model = "closed", snap = s2::s2_snap_level(30))
-sf_use_s2(FALSE)  ## Aagh don't understand
+sf_use_s2(FALSE)  ## Aagh don't understand but needed
 holyoke_roads  <- hampden_roads %>% filter(MTFCC %in% c("S1100","S1200")) %>% st_intersection(holyoke_acs2020, hampden_roads, model="open")
 
 
@@ -114,11 +117,11 @@ ggplot(data=holyoke_VTD_2020) + geom_sf() + geom_sf_text(aes(label=ward)) +
     geom_sf(data=holyoke_roads, color="pink") 
 
 ggplot(holyoke_blockgroups_acs2020) +
-    geom_sf(aes(fill=percent_families_with_children_in_poverty), lwd=0) +
+    geom_sf(aes(fill=poverty_rt_families_with_children), lwd=0) +
     scale_fill_gradient(low="gray", high="yellow") +
+    geom_sf(data=holyoke_VTD_2020, color="darkgray", fill=NA, linewidth=2) +
     geom_sf(data=holyoke_water, fill="blue") +
     geom_sf(data=holyoke_roads, color="pink") + ## + geom_sf_text(data=holyoke_roads, aes(label=FULLNAME)) +
-    geom_sf(data=holyoke_VTD_2020, color="orange", fill=NA) +
     geom_sf_text(data=holyoke_VTD_2020, aes(label=ward))
 
 
